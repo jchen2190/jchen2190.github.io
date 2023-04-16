8cc01b41507b3547b20f0aab3cf6c098ad476f3a
Jon Chen <jchen2190@gmail.com>
2023-04-16 11:52:39 -0400

    add react bootstrap + organize folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
+import './public/bootstrap/bootstrap.min.css';
 import Nav from './nav';
 import Introduction from './introduction'
 import About from './about';
 import Experience from './experience';
 import Contact from './contact';
 import Footer from './footer';
-import './style.css';
 
 function App() {
   return (
