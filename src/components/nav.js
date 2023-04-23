50c32ecd1b361b360faa1d689dbbd929a43a5dd4
Jon Chen <jchen2190@gmail.com>
2023-04-22 22:52:12 -0400

    react enable nav + add favicon

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import favicon from '../image/favicon/favicon.svg';
 
 function Navigation() {
     return (  
-        <nav className="navbar fixed-top navbar-expand-md bg-body-tertiary">
-            <div className="container-fluid">
-
-                <a className="navbar-brand" href="#introduction"><img src={favicon} width="50" height="50" alt="Jon Chen logo"/> Jon Chen</a>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse justify-content-end navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                      <li className="nav-item ms-auto"><a className="nav-link active" aria-current="page" href="#about">About</a></li>
-                      <li className="nav-item ms-auto"><a className="nav-link" href="#experience">Experience</a></li>
-                      <li className="nav-item ms-auto"><a className="nav-link" href="#projects">Projects</a></li>
-                      <li className="nav-item ms-auto"><a className="nav-link" href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-                
-            </div>
-        </nav>
+        <Navbar bg="light" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand href="#introduction"><img src={favicon} width="50" height="50" alt="Jon Chen logo"/>Jon Chen</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="me-auto">
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#experience">Experience</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
