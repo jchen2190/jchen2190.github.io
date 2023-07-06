12c38c51f1d4dbf182eecc565b2bc823d2b46d3d
Jon Chen <jchen2190@gmail.com>
2023-07-06 14:22:13 -0400

    fix spacing

--- src/components/projectOther.js
+++ src/components/projectOther.js
@@ -12,21 +12,21 @@ function OtherProjects() {
         <section id="otherprojects">
             <h2 className="title">Other Projects</h2>
             <div className="container">
-                <div className="d-flex justify-content-evenly">
+                <div className="row justify-content-evenly">
                     {
                         projects.map((project) => {
                             return (
-                                <div className="card col-4 projectText mt-4">
+                                <div className="card projectText mt-4 col-md-5 col-xl-3 mx-1">
                                     <div className="card-body">
                                         <div className="d-flex justify-content-between">
                                             <h4 className="card-title"><a href={project.link} target="_blank" rel="noopener noreferrer">{project.name}</a></h4>
                                             <a href={project.link} target="_blank" rel="noopener noreferrer"><img src={deployIcon} width="30" alt="deploy icon"/></a>
                                         </div>
                                         <p>{project.description}</p>
-                                        <div className="d-flex justify-content-between">
-                                            <p>{project.technologies}</p>
-                                            <a href={project.github} target="_blank" rel="noopener noreferrer"><img src={githubLogo} width="30" alt="github logo" /></a>
-                                        </div>
+                                    </div>
+                                    <div className="d-flex justify-content-between mx-3">
+                                        <p>{project.technologies}</p>
+                                        <a href={project.github} target="_blank" rel="noopener noreferrer"><img src={githubLogo} width="30" alt="github logo" /></a>
                                     </div>
                                 </div>
                             )
