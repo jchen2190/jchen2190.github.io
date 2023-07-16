193d576e72f2ddea93833a37417fdd68292183d7
Jon Chen <jchen2190@gmail.com>
2023-07-15 20:54:05 -0400

    format projects

--- src/components/project.js
+++ src/components/project.js
@@ -6,6 +6,33 @@ import project2 from '../image/projects/spacemission2.png'
 import project3 from '../image/projects/weatherAPI2.png'
 
 function Projects() {
+    const projects = [
+        {
+            id: "issuetracker",
+            name: "IssueTracker",
+            link: "https://issue-tracker-client-dja8.onrender.com/",
+            github: "https://github.com/jchen2190/issue-tracker-client",
+            description: "MERN (MongoDB, Express, React, NodeJS) application with CRUD (Create, Read, Update, Delete) functionality project that lists issues for users to create, edit and share issues.",
+            technologies: "JavaScript, React, MongoDB, Node.JS, Express, Bootstrap"
+        },
+        {
+            id: "spacemission",
+            name: "SpaceMission",
+            link: "https://jchen2190.github.io/spaceMission/",
+            github: "https://github.com/jchen2190/spaceMission",
+            description: "Website application made with React using NASA produced photos. User can interact with the links to see other information",
+            technologies: "JavaScript, React, Node.JS, Axios, Bootstrap"
+        },
+        {
+            id: "weatherapp",
+            name: "WeatherApp",
+            link: "https://weatherapp-2dyu.onrender.com",
+            github: "https://github.com/jchen2190/weatherAPI",
+            description: "Built a weather app that takes in a name of a city and returns the weather forecast and variables of the city location. Uses Open-Meteo Geocoding and Weather Forecast API.",
+            technologies: "JavaScript, EJS, Node.JS"
+        }
+    ]
+
     return (
         <section id="projects">
             <h2 className="title">Featured Projects</h2>
